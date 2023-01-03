6b15e6fdb8efcc25233a3e01cb21cf56c5b31952
prathmesh055 <110546808+prathmesh055@users.noreply.github.com>
2023-01-03 19:59:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,69 +1,70 @@
-img ="";
-status ="";
-object = [];
-
-function preload(){
-img = loadImage('dog_cat.jpg');    
-}
+status="";
+objects =[];
 
 function setup()
 {
-    canvas = createCanvas(640, 420);
+    canvas = createCanvas(480, 380);
     canvas.center();
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status : Dectecting Objects";
+    video= createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
 }
 
 function modelLoaded()
 {
-    console.log("Model Loaded!")
+    console.log("Model Loaded!");
     status = true;
-    objectDetector.detect(img, gotResult);
 }
 
-function gotResult(error, results)
+
+function start()
 {
-    if(error) 
-    {
-        console.log(error);
-    }
-    console.log(results);
-    object = results;
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object_name=document.getElementById("object_name").value;
 }
 
 function draw()
 {
-    image(img, 0, 0, 640, 420);
-
+    image(video, 0, 0, 480, 380);
     if(status !="")
     {
-        for(i =0; i < object.lenght; i++)
+        objectDetector.detect(video, gotResult);
+        for (i =0; i< objects.lenght; i++)
         {
-            document.getElementById("status").innerHTML ="Status : Object Detected";
+            document.getElementById("status").innerHTML = "Status : Objects Deteched";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are :"+ objects.length;
 
-            fill("#FF0000");
+            fill("FF0000");
             percent = floor(objects[i].confidence * 100);
-            text(object[i].label + " " + percent + "%", object[i].x, object[i].y);
+            text(objects[i].label +""+percent + "%",objects[i].x +15, objects[i].y +15);
             noFill();
-            stroke("#FF0000");
-            rect(objects[i].x,objects[i].y, objects[i].width,objects[i].height);
+            stroke("#ff0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            if(objects[i].label==object_name)
+            {
+                video.stop;
+                objectDetector.detect(gotResult);
+                document.getElementById("object_status").innerHTML = object_name+"found";
+                synth = window.speechSynthesis; 
+                utterThis = new SpeechSynthesisUtterance(object_name + "Found");
+                synth.speak(utterThis);
+            }
+            else
+            {
+                document.getElementById("object_status").innerHTML = object_name+"not found";
+            }
+
         }
     }
-    /*fill("#FF0000");
-    text("Dog", 45, 75); 
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350) ;
-
-    fill("#FF0000")
-    text("Cat", 320,120);
-    noFill();
-    stroke("#FF0000");
-    rect(300, 90, 270, 320);
-
-    fill("#FF0000")
-    text("Bowl", 320,320);
-    noFill();
-    stroke("#FF0000");
-    rect(270, 310, 140, 210);*/
 }
+
+function gotResult(error, results)
+  {
+    if(error)
+    {
+        console.log(error);
+   }
+   console.log(results);
+   objects = results;
+  }
